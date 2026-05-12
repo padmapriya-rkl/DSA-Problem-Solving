20bcc53f04921716ee1f639c33fb30b816bdfdf3
padmapriya-rkl <padmapriya.rkl@gmail.com>
2026-05-12 17:35:19 +0530

    Solved LeetCode 268 - Missing Number
    Implement solution for Leetcode problem 268 to find the missing number in an array.

---
+++ Arrays/MissingNumber.java
@@ -0,0 +1,22 @@
+//Leetcode 268 missing number 
+// Time Complexity: O(n)
+// Space Complexity: O(1)
+
+class Solution {
+
+    public int missingNumber(int[] nums) {
+
+        int n = nums.length;
+
+        int expected = n * (n + 1) / 2;
+
+        int actual = 0;
+
+        for(int num : nums) {
+
+            actual += num;
+        }
+
+        return expected - actual;
+    }
+}
