d546779bdb6a8763002a419f8fa3ec9e1ca6b9c4
padmapriya-rkl <padmapriya.rkl@gmail.com>
2026-05-12 11:52:45 +0530

    Solved LeetCode 283 - Move Zeroes
    Implement moveZeroes method to shift zeros in array.

---
+++ Arrays/MoveZeroes.java
@@ -0,0 +1,16 @@
+//Leetcode 283 Move Zeroes
+//2 pointer approach
+//j->points zero,i->point nonzero one 
+class Solution {
+    public void moveZeroes(int[] nums) {
+       int j=0;
+       for(int i=0;i<nums.length;i++){
+         if(nums[i] != 0) {
+
+                int temp = nums[j];
+                nums[j] = nums[i];
+                nums[i] = temp;
+
+                j++;
+       } 
+    }}}
